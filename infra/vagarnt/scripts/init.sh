a384a43f103cfce39f3892183346666c0b577512
유시환 <tlghks1217@5finger.co.kr>
2025-11-06 00:50:11 +0900

    [ã[init] Ansible 설정 추가 및 vagrant 호스트 VM 게스트 í프로비저닝

--- infra/vagarnt/scripts/init.sh
+++ infra/vagarnt/scripts/init.sh
@@ -5,7 +5,10 @@ echo "[INIT] Updating system..."
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-echo "[INIT] Basic utilities install..."
-sudo apt-get install -y curl wget git vim net-tools
+echo "[INIT] Installing basic utilities..."
+sudo apt-get install -y curl wget git vim net-tools software-properties-common
+
+echo "[INIT] Installing Ansible..."
+sudo apt-get install -y ansible
 
 echo "[INIT] VM setup complete."
